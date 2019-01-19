2e15fb96ed6a78c674f71ff79fb7517ea565747d
Christian Cashiola <cashiolachristian@gmail.com>
2019-01-19 14:49:20 -0500

    Setup reducers and store

--- frontend/stellar.jsx
+++ frontend/stellar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Stellar</h1>, root);
+  ReactDOM.render(<h1>hey</h1>, root);
 });
