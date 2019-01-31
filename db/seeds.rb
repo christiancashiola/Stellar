e86abeee16e5cefdce1d4bec27e9dc0104b5833c
Christian Cashiola <cashiolachristian@gmail.com>
2019-01-31 16:21:03 -0500

    Ready seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,9 @@ Post.destroy_all
 Tag.destroy_all
 Like.destroy_all
 
-# Text
-b1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer orci lorem, dapibus at nulla ac, accumsan fermentum velit. Cras dignissim sapien nisi, quis pharetra turpis scelerisque vel. Curabitur libero sem, euismod at neque a, condimentum consequat elit. Ut neque lectus, tempus ut massa vitae, consectetur sollicitudin arcu. Nullam at ex id felis interdum sagittis vitae vitae nibh. Nam quis tellus eget arcu bibendum bibendum non a eros. Praesent sit amet finibus elit, in luctus leo. Sed congue erat sed justo sollicitudin, at commodo tellus volutpat. Nunc leo ex, lobortis id ultrices ut, rutrum vel arcu. Etiam dignissim dui ut turpis."
-b2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut congue lectus. Ut malesuada tempor metus vel elementum. Sed nec convallis velit. Aliquam consequat laoreet urna. Ut ut erat pretium, facilisis neque ac, vehicula leo. Curabitur accumsan finibus pharetra. Ut ultrices dui vel dapibus fermentum. Nulla elementum porta maximus. Pellentesque."
-b3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis, arcu id finibus feugiat, ex turpis molestie nibh, at finibus mi erat et tellus. Curabitur."
-bodies = [b1, b2, b3]
-
-# TAGS
-t1 = Tag.create!({subject: '#food'})
-t2 = Tag.create!({subject: '#anime'})
-t3 = Tag.create!({subject: '#trails'})
-t4 = Tag.create!({subject: '#running'})
-t5 = Tag.create!({subject: '#gaming'})
-t6 = Tag.create!({subject: '#music'})
-t7 = Tag.create!({subject: '#life'})
-t8 = Tag.create!({subject: '#reading'})
-t9 = Tag.create!({subject: '#coffee'})
-
 # USERS
-ud = User.create!({ email: 'demo@demo.com', password: '12345678', username: 'demo_user19', admin: true })
+ud = User.create!({ email: 'demo@demo.com', password: '12345678', username: 'demo_user19' })
+admin = User.create!({ email: 'admin@demo.com', password: '12345678', username: 'admin', admin: true })
 u1 = User.create!({ email: 'user1@demo.com', password: '12345678', username: 'itsK' })
 u2 = User.create!({ email: 'user2@demo.com', password: '12345678', username: 'feltfanatic' })
 u3 = User.create!({ email: 'user3@demo.com', password: '12345678', username: 'ZRobot' })
