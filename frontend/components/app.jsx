273446c36c2760d396b432deb3d737f774f8ad12
Christian Cashiola <cashiolachristian@gmail.com>
2019-02-05 10:24:56 -0500

    Add custom ping route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,13 +6,17 @@ import Dashboard from './dashboard/dashboard';
 import Nav from './nav/nav_container';
 import Modal from './modals/modal_container';
 import ExploreSearch from './explore_search/explore_search';
+import Ping from './ping/ping';
 
 const App = () => {
   return (
     <>
       <Modal />
       <Route path='/' component={Nav} />
-      <AuthRoute exact path='/' component={Splash} />
+      <Switch>
+        <Route exact path='/ping/pong' component={Ping} />
+        <AuthRoute exact path='/' component={Splash} />
+      </Switch>
       <Switch>
         <ProtectedRoute path='/search/:tag' component={ExploreSearch} />
         <ProtectedRoute path='/dashboard' component={Dashboard} />
---
+++ frontend/components/ping/ping.jsx
@@ -0,0 +1,16 @@
+import React from 'React';
+
+export default () => {
+
+  const styles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '50px',
+    color: '#ccc'
+  }
+
+  return(
+    <h1 style={styles}>Ping!</h1>
+  );
+};
